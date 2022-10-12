9f8dec1593af3e7b75e9c0e26f3c3ace5bdecc38
songyahui <songyahuiapp@hotmail.com>
2022-10-12 11:28:59 +0800

    m

--- code/src/program/fischer.c
+++ code/src/program/fischer.c
@@ -2,8 +2,8 @@ x := -1;
 cs:= 0;
 
 void proc (int i) 
-/* req: (e>0∧d>e) ∧ ((_)^*)
-   ens:  e>0∧d>e∧f0≥0∧f0≤d∧f1=e ∧ (([x=-1]? ((Update(i){x:=i})#f0) . ((ε)#f1) . ([x=i] . Critical(i){cs:=(cs+1)} . Exit(i){cs:=cs-1;x:=-1} + [(~(x=i))]) )^*)*/
+/* req: (e>0∧d<e) ∧ ((_)^*)
+   ens:  e>0∧d<e∧f0≥0∧f0≤d∧f1=e ∧ (([x=-1]? ((Update(i){x:=i})#f0) . ((ε)#f1) . ([x=i] . Critical(i){cs:=(cs+1)} . Exit(i){cs:=cs-1;x:=-1} + [(~(x=i))]) )^*)*/
 {
  [x=-1] //block waiting until true
  deadline(event["Update"(i)]{x:=i},d);
@@ -17,8 +17,8 @@ void proc (int i)
   }}
 
 void main (int i)
-/* req: (e>0∧d>e) ∧ ((_)^*)
-   ens: (e>0∧d>e) ∧ (([cs≤1])^*) */
+/* req: (e>0∧d<e) ∧ ((_)^*)
+   ens: (e>0∧d<e) ∧ (([cs≤1])^*) */
 {
   proc(0) 
 
