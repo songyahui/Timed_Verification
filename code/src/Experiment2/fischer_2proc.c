442df0401296fd79d01fc4c0207cc4f0b829f928
songyahui <songyahuiapp@hotmail.com>
2022-10-13 12:38:29 +0800

    m

--- code/src/Experiment2/fischer_2proc.c
+++ code/src/Experiment2/fischer_2proc.c
@@ -4,9 +4,17 @@ cs:= 0;
 void proc (int i) 
 /* req: (d<e) ∧ ((_)^*)
    ens:  (d<e∧f0≤d∧f1=e) ∧ (([x=-1]? ((Update(i){x:=i})#f0) . ((ε)#f1) . ([x=i] . Critical(i){cs:=(cs+1)} . Exit(i){cs:=cs-1;x:=-1} + [(~(x=i))]) ))*/
-{()}
-
-//
+{
+ [x=-1] //block waiting until true
+ deadline(event["Update"(i)]{x:=i},d);
+   delay (e);
+   if (x==i) {
+     event["Critical"(i)]{cs:=(cs+1)};
+     event["Exit"(i)]{cs:=cs-1;x:=-1};
+     //proc (i);
+   } else {
+    () //proc (i);
+  }}
 
 
 void main ()
---
+++ code/src/Experiment2/fischer_6proc_wrong.c
@@ -0,0 +1,34 @@
+x := -1; 
+cs:= 0;
+
+void proc (int i) 
+/* req: (d<e) ∧ ((_)^*)
+   ens:  (d<e∧f0≤d∧f1=e) ∧ (([x=-1]? ((Update(i){x:=i})#f0) . ((ε)#f1) . ([x=i] . Critical(i){cs:=(cs+1)} . Exit(i){cs:=cs-1;x:=-1} + [(~(x=i))]) ))*/
+{
+ [x=-1] //block waiting until true
+ deadline(event["Update"(i)]{x:=i},d);
+   delay (e);
+   if (x==i) {
+     event["Critical"(i)]{cs:=(cs+1)};
+     event["Exit"(i)]{cs:=cs-1;x:=-1};
+     //proc (i);
+   } else {
+    () //proc (i);
+  }}
+
+
+void main ()
+/* req: (e>0∧d>e) ∧ ε
+ens: (d>e) ∧ ([cs≤1]^*)
+  */
+{
+  proc(0) ||
+    proc(1) ||
+    proc(2)
+    ||
+    proc(3)||
+    proc(4)
+        ||
+    proc(5)
+
+}
